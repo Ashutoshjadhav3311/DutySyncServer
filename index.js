602cb3b77497315daf78225a1b83e09861b565b0
Ashutosh Jadhav <104265231+Ashutoshjadhav3311@users.noreply.github.com>
2024-02-22 13:00:46 +0000

    Update index.js

--- index.js
+++ index.js
@@ -60,7 +60,7 @@ app.post('/createHouse', async (req, res) => {
 
 
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 9000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });

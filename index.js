64d8b8db0cf474f51f1579968e6d46532f2d6efa
AshutoshJadhav01 <x22183868@student.ncirl.ie>
2024-02-27 00:11:44 +0000

    Save roles

--- index.js
+++ index.js
@@ -1,67 +1,167 @@
 
-  const { MongoClient, ServerApiVersion,ObjectId } = require('mongodb');
-  const cors = require('cors');
-  const express = require('express');
-  const app = express();
-  // Middleware to parse JSON request body
-  app.use(express.json());
-  app.use(cors({
-    origin: 'http://localhost:3000'
-  }));
-  // Create a MongoClient with a MongoClientOptions object to set the Stable API version
-  const uri = "mongodb+srv://jadhavashu1m:Thuglife@cluster0.hgwnc0j.mongodb.net/?retryWrites=true&w=majority";
-  const client = new MongoClient(uri);
+const { MongoClient, ServerApiVersion,ObjectId } = require('mongodb');
+const cors = require('cors');
+const express = require('express');
+const app = express();
+// Middleware to parse JSON request body
+app.use(express.json());
+app.use(cors({
+  origin: ['http://localhost:3000', 'https://dutysync.netlify.app']
+}));
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const username = process.env.MONGODB_USERNAME;
+const password = process.env.MONGODB_PASSWORD;
+
+
+const uri = `mongodb+srv://${username}:${password}@cluster0.hgwnc0j.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri);
 
 
 
 
-  async function main() {
-    try {
-      // Connect the client to the server	(optional starting in v4.7)
-      await client.connect();
-      //await listDatabases(client);
-      // Send a ping to confirm a successful connection
-      //await client.db("admin").command({ ping: 1 });
-      console.log(" You successfully connected to MongoDB!");4
-      
-    } finally {
-      // Ensures that the client will close when you finish/error
-      //await client.close();
-    }
-  }
-  async function createHouse(client, newHouse){
-    const result = await client.db("DutySyncHouse").collection("HouseList").insertOne( newHouse );
-    console.log("name of the house is",newHouse )
-    console.log(`New listing created with the following id: ${result.insertedId}`);
+async function main() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    //await listDatabases(client);
+    // Send a ping to confirm a successful connection
+    //await client.db("admin").command({ ping: 1 });
+    console.log(" You successfully connected to MongoDB!");4
+    const PORT = process.env.PORT || 9000;
+app.listen(PORT, () => {
+console.log(`Server is running on port ${PORT}`);
+});
+} catch (error) {
+  console.error("Error connecting to MongoDB:", error);
+}
 }
 
-async function listDatabases(client) {
-  databasesList = await client.db().admin().listDatabases();
+  
 
-  console.log("Databases:");
-  databasesList.databases.forEach(db => console.log(` - ${db.name}`));
+async function listDatabases(client) {
+databasesList = await client.db().admin().listDatabases();
+
+console.log("Databases:");
+databasesList.databases.forEach(db => console.log(` - ${db.name}`));
 };
 
+async function addHouseMember(client, newHouse){
+  console.log("name of the house is",newHouse.Housename)
+  console.log("name of the Member is",newHouse.Membername)
+  const result=await client.db("DutySyncHouse").collection("HouseList").findOneAndUpdate(
+    { Housename: newHouse.Housename }, 
+  { $push: { Membername: newHouse.Membername } }, 
+  { new: true },)
+  console.log('addhousemember housename is :',newHouse.Housename)
+  console.log('addhousemember membername is :',newHouse.Membername)
+  console.log('addhousemember:',result)
+}
 
+async function getHouseMembers(client, houseName){
+  const result=await client.db("DutySyncHouse").collection("HouseList").findOne({ Housename: houseName });
+  return result;
+}
+
+async function checkMember(client, membername){
+  const result=await client.db("DutySyncHouse").collection("HouseList").findOne({ Membername: membername });
+  return result;
+}
 //APIs
-app.post('/createHouse', async (req, res) => {
-  try {
-    const  houseData  = req.body;
-    console.log  (houseData);
-    // Call createHouse function to insert the new house data
-    await createHouse(client, houseData);
+// Ping endpoint to keep server active on render
+app.get('/ping', (req, res) => {
+res.status(200).send('Server is alive!');
+});
 
-    res.status(201).json({ message: houseData });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: 'Internal server error :Coudld not add houseName' });
+app.get('/checkHouseMember/:membername',async(req,res)=>{
+  try{
+    const membername = req.params.membername;
+    console.log('Membername:',membername)
+    const result = await checkMember(client, membername);
+    if(result==null){
+      res.status(400).json("Member not in any house");
+      return;
+    }
+    res.status(200).json(result);
+      console.log('getHouseMembers:',result.Housename)
+    
   }
+  catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal server error :Coudld not get house members' });
+}});
+
+app.get('/getHouseMembers/:houseName', async (req, res) => {
+try{
+  const houseName = req.params.houseName;
+  console.log('houseName:',houseName)
+  const result = await getHouseMembers(client, houseName);
+  res.status(200).json(result);
+  console.log('getHouseMembers:',result)
+}
+catch (error) {
+  console.error(error);
+  res.status(500).json({ error: 'Internal server error :Coudld not get house members' });
+}});
+
+app.post('/createHouse', async (req, res) => {
+try {
+  const  houseData  = req.body;
+//  console.log  (houseData);
+  const housename= houseData.Housename
+ // console.log(housename)
+  const existing= await client.db("DutySyncHouse").collection("HouseList").findOne( {Housename:housename} );
+  
+  console.log("existing",existing)
+  if (existing){
+    return res.status(400).json({ message: 'Data already exists' });
+}
+const result = await client.db("DutySyncHouse").collection("HouseList").insertOne( houseData );
+const insert=await client.db("DutySyncHouse").collection("Role").insertOne( { Housename: housename } );
+// add housename in roles collection as well
+//  console.log(`New listing created with the following id: ${result.insertedId}`);
+  res.status(201).json({ message: 'Data saved successfully' });
+  } 
+ catch (error) {
+  console.error(error);
+  res.status(500).json({ error: 'Internal server error :Coudld not add houseName' });
+}
 });
 
+app.post('/addHouseMember', async (req, res) => {
+try {
+  const  houseData  = req.body;
+  console.log  (houseData);
+  await addHouseMember(client, houseData);
+  // Call createHouse function to insert the new house data
+  
 
-
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  res.status(201).json({ message: houseData });
+} catch (error) {
+  console.error(error);
+  res.status(500).json({ error: 'Internal server error :Coudld not add housemember' });
+}
 });
-  main().catch(console.dir);
+
+app.post('/saveRoles',async(req,res)=>{
+  try {
+  const  roles  = req.body;
+  const result=await client.db("DutySyncHouse").collection("Role").findOneAndUpdate(
+    { Housename: roles.Housename }, 
+  {$set: { RolesNames: roles.RolesNames } },    //$set overwrites the existing data
+  { new: true },) 
+  console.log(roles)
+  console.log(result)
+  if(result){
+    return res.status(201).json({ message: 'Roles saved sucessfully' });
+  }
+   
+   res.status(400).json({ message: 'housename not present in database' });
+}
+   catch(error){
+    res.status(500).json({ error: 'Internal server error :Coudld not add roles' });
+   }
+})
+
+main().catch(console.dir);
+module.exports = app;
